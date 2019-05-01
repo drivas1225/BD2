7840d9bfdd18e9aac93277f20222182966722df8
FlechitUp <ximena.pocco@ucsp.edu.pe>
2019-05-01 09:20:15 -0500

    select for colums

--- final_project.py
+++ final_project.py
@@ -1,5 +1,6 @@
 from shutil import copyfile
 from random import randint, uniform, random
+import csv
 """
     para copiar archivos se usa esa libreria con el comando
     copyfile(fuente_origen, fuente_destino)
@@ -11,23 +12,23 @@ def tablaNueva(nombre, columnas, tiposCols):
     ruta = 'BD/' + nombre + '.mtd'
     archivo = open(ruta, 'w')
 
-    textoMetadata = '--MTD,'+str(len(columnas))+',0,'
+    textoMetadata = '--MTD,' + str(len(columnas)) + ',0,'
     for cols in columnas:
-        textoMetadata += str(cols)+','
+        textoMetadata += str(cols) + ','
     for tcols in tiposCols:
-        textoMetadata += str(tcols)+','
+        textoMetadata += str(tcols) + ','
         #archivo.write(cols + '\n')
     textoMetadata += 'MTD--'
     archivo.write(textoMetadata)
     archivo.close()
 
-    #Crea tabla 
+    #Crea tabla
     ruta = 'BD/' + nombre + '.txt'
     archivo = open(ruta, 'w')
     texto = ''
     for cols in columnas:
         texto += str(cols) + ','
-    texto = texto[:len(texto)-1]
+    texto = texto[:len(texto) - 1]
     archivo.write(texto)
     archivo.close()
     print('tabla creada correctamente!')
@@ -36,9 +37,11 @@ def tablaNueva(nombre, columnas, tiposCols):
 def insertar(nombre, elementos):
     ruta = 'BD/' + nombre + '.txt'
     archivo = open(ruta, 'a')
+    texto = ''
     for elemento in elementos:
-        archivo.write(elemento + ',')
-    archivo.write('\n')
+        texto = elemento + ','
+    texto = texto[:len(texto) - 1]
+    archivo.write(texto+'\n')
     archivo.close()
     print("insertado!")
 
@@ -91,13 +94,24 @@ def borrar(nombre, condicion):
 def selectA(nombreTabla, cols='*'):
     ruta = 'BD/' + nombreTabla + '.txt'
     archivo = open(ruta, 'r')
-    lineas = archivo.readlines()
-
+    
     if(cols == '*'):
+        lineas = csv.reader(archivo)
         for linea in lineas:
             print(linea)
-    #else:
+    else:
+        #posCols = comprobarCols(nombreTabla)
+        lineas = csv.DictReader(archivo)
+        print(cols)
+        text=''
+        for linea in lineas:
+            for col in cols:                
+                #print(col)
+                text += linea[col]+" "
+            print(text)
     archivo.close()
+
+
 
 def select(nombre, condicion):
     ruta = 'BD/' + nombre + '.txt'
@@ -221,13 +235,73 @@ def update(nombre, actualizacion, condicion):
     archivo.close()
     print("actualizado!")
 
+
 def verificarTipo(tipo):
     #print("tipo ", tipo)
     if tipo == 'int' or 'varchar':
         return True
     else:
-        print(' * Error tipo ',tipo) 
+        print(' * Error tipo ', tipo)
         return False
+
+def getPosAND_OR(strComm):
+    posA = strComm.find('AND')
+    posO = strComm.find('OR')
+    return min(posA, posO) if min(posA, posO)>0 else max(posO,posA)
+
+def formatearConditions(strComm):
+    #print(strComm)
+    coumt = 1
+    posAND_OR = getPosAND_OR(strComm)
+    i = 0
+
+    if posAND_OR > 0:
+        coumt = 2
+    else: posAND_OR = len(strComm)
+
+    while(i < posAND_OR and coumt>0):
+        p = strComm.find('>', i,posAND_OR)
+        if (p >0):
+            if strComm[p] != ' ':
+                strComm = strComm[:p]+" "+strComm[p:]                
+            if strComm[p + 1] != ' ':
+                strComm = strComm[:p+2]+" "+strComm[p+2:]
+                print('a',strComm)
+            i = posAND_OR
+            posAND_OR = len(strComm)
+            coumt -=1
+        else:
+            p = strComm.find('<',i,posAND_OR)
+            if (p > 0):
+                if strComm[p] != ' ':
+                    strComm = strComm[:p] + " " + strComm[p:]
+                if strComm[p + 1] != ' ':
+                    strComm = strComm[:p + 2] + " " + strComm[p + 2:]
+                i = posAND_OR
+                posAND_OR = len(strComm)
+                coumt-=1
+            else:
+                p = strComm.find('=',i,posAND_OR)
+                if (p > 0):
+                    if strComm[p] != ' ':
+                        strComm = strComm[:p] + " " + strComm[p:]
+                    if strComm[p + 2] != ' ':                        
+                        strComm = strComm[:p + 2] + " " + strComm[p+2:]                    
+                    i = posAND_OR
+                    posAND_OR = len(strComm)
+                    coumt-=1
+    #print(": ", strComm)
+
+    return strComm
+
+def getConditions(strComm):
+    strComm = formatearConditions(strComm)
+    ArrComm = strComm.split()
+    print(ArrComm)
+
+"""def getNameTable(commOri, keyWord):
+    posF = commOri.find(keyWord)
+    return comandoOrig[:posF-1]"""
 
 def printHelp():
     print("COMANDOS:")
@@ -237,6 +311,7 @@ def printHelp():
     print("SELECCIONA [tabla] DONDE [condicion]")
     print("update [tabla] set [a_actualizar] DONDE [condicion]")
 
+
 # elementos = []
 # elementos.append('0')
 # elementos.append('nombre_x')
@@ -245,20 +320,23 @@ def printHelp():
 while(1):
     #printHelp()
     #comandoOrig = 'CREA_TABLA a(a1, int; a2 , int; a3, int)'  # input()
-    comandoOrig = 'INSERTA a (a1, a2, a3) VALORES (10,8,4)'  # input()
+    #comandoOrig = 'INSERTA a (a1, a2, a3) VALORES (-1,8,4)'  # input()
     #comandoOrig= 'SELECCIONA * DESDE a;'  # input()
+    #comandoOrig= 'SELECCIONA a1, a3 DESDE a;'  # input()
+    comandoOrig = 'SELECCIONA a1, a3 DESDE a DONDE a1> 0 AND a2<8 ;'  # input()
     print(comandoOrig)
+    comandoOrig = comandoOrig[:len(comandoOrig) - 1]  # quita ; final
     comando = comandoOrig.split()
     size = len(comando)
-    comando[size - 1] = comando[size - 1].replace(';', '')  # quita ; final
+    #comando[size - 1] = comando[size - 1].replace(';', '')  # quita ; final
     # create table [nombre] (columna tipo);
     if comando[0] == 'CREA_TABLA':
-        nombreTabla = comando[1]                
+        nombreTabla = comando[1]
         cols = []
         tiposCols = []
         cols2 = []
         comando[2] = comando[2][1:]  # borra (
-        print("ALL C",comando)
+        print("ALL C", comando)
         """for i in range(2, size-1,2):
             print(comando[i].replace(',', ''))
             cols.append(comando[i].replace(',', ''))
@@ -266,20 +344,20 @@ while(1):
         tablaNueva(nombreTabla, cols, tiposCols)"""
 
         cols = comandoOrig[comandoOrig.find('(') + 1:comandoOrig.find(')')]
-        cols = cols.split(';')    
-        #print("las" ,(cols))        
-        for i in range(0, len(cols)):        
+        cols = cols.split(';')
+        #print("las" ,(cols))
+        for i in range(0, len(cols)):
             tC = (cols[i].split(','))
             for j in range(len(tC)):
-                tC[j] = tC[j].strip()  #strip() quita espacios blamco
+                tC[j] = tC[j].strip()  # strip() quita espacios blamco
             #print("TC",tC)
             if (not(verificarTipo(tC[1]))):
                 break
-            else: 
+            else:
                 cols2.append(tC[0])
                 tiposCols.append(tC[1])
         tablaNueva(nombreTabla, cols2, tiposCols)
-                
+
     # insert [tabla] (a1, a2, a2 )VALORES (v1, v2, v3);
     elif comando[0] == 'INSERTA':
         nombreTabla = comando[1]
@@ -291,9 +369,10 @@ while(1):
         insertar(nombreTabla, elms)"""
         cols = comandoOrig[comandoOrig.find('(') + 1:comandoOrig.find(')')]
         cols = cols.split(',')
-        posVal = comandoOrig.find('VALORES') 
-        if (posVal >0):
-            values = comandoOrig[comandoOrig.find('(', posVal + 1)+1: comandoOrig.find(')',len(comandoOrig) )]
+        posVal = comandoOrig.find('VALORES')
+        if (posVal > 0):
+            values = comandoOrig[comandoOrig.find(
+                '(', posVal + 1) + 1: comandoOrig.find(')', len(comandoOrig))]
             values = values.split()
         print(cols)
         print(values)
@@ -317,29 +396,34 @@ while(1):
             cndn.append(comando[i])
         borrar(nombreTabla, cndn)
 
-    # select * DESDE [tabla]; 
+    # select * DESDE [tabla];
     # select c1,c2 DESDE [tabla] DONDE [condicion]
     # select c1,c2 DESDE [tabla] DONDE [condicion] and [condicion]
-    
+
     elif comando[0] == 'SELECCIONA':  # considerar que la condicion va separada por ' '
-        nombreTabla = comando[3]
-        print(comando[1])
-        print(comando[2])
-        print(comando[3])
+        nombreTabla = ''        
         colsSelect = []
-        if (comando[1] =='*'):
+        if (comando[1] == '*'):
             nombreTabla = comando[3]
             selectA(nombreTabla)
-        else: 
-            print("HHEHEE")
-            i = 1            
-            while(comando[i] != 'DESDE' and i < len(comando)):
-                print(comando[i])
-                colsSelect.append(comando[i])
-                i+=1
-            selectA(nombreTabla,colsSelect)
-            #case de where
-            #print(colsSelect)    
+        else:                     
+            posFim = comandoOrig.find('DESDE')
+            colsSelect = comandoOrig[comandoOrig.find('SELECCIONA') + 11: posFim-1]
+            colsSelect = colsSelect.split(',') 
+            posDomde = comandoOrig.find('DONDE')           
+            
+            if (posDomde <0):                
+                nombreTabla = comandoOrig[posFim + 6:]
+                #print(colsSelect)
+                for j in range(len(colsSelect)):
+                    colsSelect[j] = colsSelect[j].strip()  # strip() quita espacios blamco
+                selectA(nombreTabla, colsSelect)
+
+            else:  # case de where
+                nombreTabla = comandoOrig[posFim + 6:posDomde].strip()                
+                strConditions = comandoOrig[posDomde + 5:]
+                getConditions(strConditions)
+                #print(colsSelect)
             """cndn = []
             for i in range(3, size):
                 cndn.append(comando[i])
